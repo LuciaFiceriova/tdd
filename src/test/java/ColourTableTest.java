832021171f95da02a44db068a60232dc57971960
LuciaFiceriova <lucia.ficeriova@gmail.com>
2023-11-26 17:54:25 +0000

    Changed testNumOfColours so it will succeed

--- src/test/java/ColourTableTest.java
+++ src/test/java/ColourTableTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 public class ColourTableTest {
     @Test
     public void testNumOfColours(){
-        int numOfColours = 20;
+        int numOfColours = 16;
         ColourTable colourTable = new ColourTable(numOfColours);
         Assertions.assertNotNull(colourTable, "ColourTable object should not be null");
     }
