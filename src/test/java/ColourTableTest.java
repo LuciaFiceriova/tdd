5696e4ce1a53261b7e924270cb00806b20659af3
LuciaFiceriova <lucia.ficeriova@gmail.com>
2023-11-26 17:53:42 +0000

    Added a Test for constructor which will fail

--- src/test/java/ColourTableTest.java
+++ src/test/java/ColourTableTest.java
@@ -1,2 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ColourTableTest {
+    @Test
+    public void testNumOfColours(){
+        int numOfColours = 20;
+        ColourTable colourTable = new ColourTable(numOfColours);
+        Assertions.assertNotNull(colourTable, "ColourTable object should not be null");
+    }
 }
