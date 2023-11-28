074493fa6ce631f91d0a6832b42470e7f72714c8
LuciaFiceriova <lucia.ficeriova@gmail.com>
2023-11-28 16:45:24 +0000

    Added a Test for an add method which will fail

--- src/test/java/ColourTableTest.java
+++ src/test/java/ColourTableTest.java
@@ -8,4 +8,21 @@ public class ColourTableTest {
         ColourTable colourTable = new ColourTable(numOfColours);
         Assertions.assertNotNull(colourTable, "ColourTable object should not be null");
     }
+
+    @Test
+    public void testAdd() {
+        int capacity = 4;
+        ColourTable colourTable = new ColourTable(capacity);
+
+        int red = 0xFF0000;
+        int green = 0x00FF00;
+        int blue = 0x0000FF;
+
+        colourTable.add(red);
+        colourTable.add(green);
+        colourTable.add(blue);
+
+        Assertions.assertEquals(capacity, colourTable.getNumOfColoursAdded(), "Number of colours added to the table should not exceed the capacity");
+    }
+
 }
