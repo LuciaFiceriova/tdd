c920b9ad64150c39b653bf0f7b0bb76edd5656d9
LuciaFiceriova <lucia.ficeriova@gmail.com>
2023-12-01 17:41:31 +0000

    After running the Test, I have expanded the constructor method, added isValidPaletteSize method and getSize method

--- src/main/java/assignment3/ColourTable.java
+++ src/main/java/assignment3/ColourTable.java
@@ -1,9 +1,21 @@
 package assignment3;
 
-
 public class ColourTable {
+    private final int size;
 
     public ColourTable(int size) {
+        if (!isValidPaletteSize(size)) {
+            throw new InvalidSizeException();
+        }
+        this.size = size;
+    }
+
+    public boolean isValidPaletteSize(int size) {
+        return size > 1 && size <= 1024 && (size & (size - 1)) == 0;
+    }
+
+    public int getSize() {
+        return size;
     }
 
 }
